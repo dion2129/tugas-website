1458dca85caba78d8f91721704b986c2e70c36d9
Ahmad <82072585+AhmadMifta@users.noreply.github.com>
2025-05-25 14:57:21 +0800

    team.php

--- Page/team.php
+++ Page/team.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Tim Hebat</title>
+  <title>Team</title>
+  <link rel="icon" href="images/favicon.png" type="image/x-icon">
   <link rel="stylesheet" href="../plugins/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="../plugins/node_modules/bootstrap-icons/font/bootstrap-icons.css">
 </head>
