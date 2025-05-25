59365e6b44b9dbeaad62ed6aaa1afd9b4cfc5208
Ahmad <82072585+AhmadMifta@users.noreply.github.com>
2025-05-25 14:56:59 +0800

    index.php

--- Page/index.php
+++ Page/index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Medical Homepage</title>
+  <link rel="icon" href="images/favicon.png" type="image/x-icon">
   <link rel="stylesheet" href="../plugins/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="../plugins/node_modules/bootstrap-icons/font/bootstrap-icons.css">
 </head>
