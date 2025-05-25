c3f183c5a75ef844421fdf99d3d66826e10c0a57
Ahmad <82072585+AhmadMifta@users.noreply.github.com>
2025-05-25 14:45:24 +0800

    index.php

--- Page/index.php
+++ Page/index.php
@@ -4,7 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Medical Homepage</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../plugins/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../plugins/node_modules/bootstrap-icons/font/bootstrap-icons.css">
 </head>
 <body>
 
@@ -455,6 +456,7 @@
   </div>
 </section>
 
+    <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- Footer -->
 <?php 
 include "../component/Footer.php"
